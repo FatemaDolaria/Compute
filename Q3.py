0526d18c12acbd85127578e19b9d709e66fc8c34
FatemaDolaria <99170834+FatemaDolaria@users.noreply.github.com>
2022-10-13 16:23:02 +0530

    Delete Q3.py

--- Q3.py
+++
@@ -1,7 +0,0 @@
-'''Given a permutation ‘a’ of length ‘n’, your task is to find out a lexicographically smallest 
-permutation ‘b’ of length ‘n’ such that it is different from ‘a’ at every position 
-i.e. a[1] ≠ b[1], a[2] ≠ b[2],.....,a[n]≠ b[n]'''
-
-n = int(input())
-if n == 1 :
-    print('-1')
